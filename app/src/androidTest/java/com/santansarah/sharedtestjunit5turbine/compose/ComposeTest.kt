20bbbef623392a2c55f1d53317fc8fbbe276ad32
sarah <santansarah@gmail.com>
2023-02-27 14:58:52 -0700

    Bumped JVM to 11 for Compose JUnit 5 testing.

--- app/src/androidTest/java/com/santansarah/sharedtestjunit5turbine/compose/ComposeTest.kt
+++ app/src/androidTest/java/com/santansarah/sharedtestjunit5turbine/compose/ComposeTest.kt
@@ -1,24 +1,25 @@
 package com.santansarah.sharedtestjunit5turbine.compose
 
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.onNodeWithText
 import com.santansarah.sharedtestjunit5turbine.Greeting
+import com.santansarah.sharedtestjunit5turbine.MainActivity
 import com.santansarah.sharedtestjunit5turbine.ui.theme.SharedTestJUnit5TurbineTheme
+import de.mannodermaus.junit5.compose.createAndroidComposeExtension
 import de.mannodermaus.junit5.compose.createComposeExtension
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 
 class ComposeTest {
 
-    // Cannot inline bytecode built with JVM target 11 into bytecode that is being built with JVM
-    // target 1.8. Please specify proper '-jvm-target' option
-    //val extension = createAndroidComposeExtension<MainActivity>()
-
     @RegisterExtension
     @JvmField
-    val extension = createComposeExtension()
+    val extension = createAndroidComposeExtension<ComponentActivity>()
+
+    //val extension = createComposeExtension()
 
     @Test
     fun test_Greeting() {
